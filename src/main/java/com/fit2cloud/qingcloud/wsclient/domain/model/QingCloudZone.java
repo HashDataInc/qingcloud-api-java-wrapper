ec55a3a0d0e82d11df0f595e60d73479714b4f0b
Zhanwei Wang <wang@hashdata.cn>
2017-08-15 19:34:03 +0800

    add new zone

--- src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudZone.java
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudZone.java
@@ -3,7 +3,9 @@ package com.fit2cloud.qingcloud.wsclient.domain.model;
 public class QingCloudZone {
 	public final static String PEK1 = "pek1";
     public final static String PEK2 = "pek2";
-    public final static String PEK3 = "pek3a";
+    public final static String PEK3a = "pek3a";
+    public final static String PEK3b = "pek3b";
+    public final static String SH1a = "sh1a";
     public final static String GD1 = "gd1";
     public final static String AP1 = "ap1";
 }
