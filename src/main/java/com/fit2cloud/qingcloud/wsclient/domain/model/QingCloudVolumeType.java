1f9e348d739c8e40c8ea0c549812aa64023255b9
Zhanwei Wang <wang@hashdata.cn>
2017-08-15 20:50:25 +0800

    add super performance volume type

--- src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudVolumeType.java
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudVolumeType.java
@@ -3,4 +3,5 @@ package com.fit2cloud.qingcloud.wsclient.domain.model;
 public class QingCloudVolumeType {
 	public final static int PERFORMANCE = 0;
 	public final static int CAPACITY = 1;
+	public final static int PERFORMANCE_SUPER = 3;
 }
